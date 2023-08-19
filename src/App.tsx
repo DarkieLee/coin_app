0f82cf29a7d80e2270bb1f953a772a5c29aca5ce
Darkie Lee <ducle0906.ulis@gmail.com>
2023-08-19 15:24:37 +0700

    modify app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import { useState } from 'react'
+import NavBar from './components/navbar'
+import Home from './components/home'
+import Market from './components/market'
+import ChooseUs from './components/chooseUs'
+import JoinUs from './components/joinUs'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-  <h1>Hi</h1>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/market" element={<Market />} />
+        <Route path="/chooseus" element={<ChooseUs />} />
+        <Route path="/joinus" element={<JoinUs />} />
+      </Routes>
+    </>
   )
 }
 
