50b50892cd81b79f770326f3d531d5cb3a71e6d8
Darkie Lee <ducle0906.ulis@gmail.com>
2023-08-20 17:42:01 +0700

    modify home to get error alert

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -14,10 +14,14 @@ function Home() {
 
     useEffect(() => {
         async function fetchData() {
-            const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false&locale=en")
-            const result: Obj[] = await res.json()
-            console.log(result)
-            setData(result)
+            try {
+                const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false&locale=en")
+                const result: Obj[] = await res.json()
+                console.log(result)
+                setData(result)
+            }catch(error){
+                alert(error)
+            }
         };
         fetchData();
     }, []);
